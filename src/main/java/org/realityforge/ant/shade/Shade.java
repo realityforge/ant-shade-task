66e2efb6e9d76fbf854595e39cc260b9eb0d0011
Peter Donald <peter@realityforge.org>
2017-11-29 09:09:48 +1100

    Whitespace

--- src/main/java/org/realityforge/ant/shade/Shade.java
+++ src/main/java/org/realityforge/ant/shade/Shade.java
@@ -7,7 +7,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.shade.DefaultShader;
 import org.apache.maven.plugins.shade.ShadeRequest;
@@ -19,166 +18,190 @@ import org.apache.maven.plugins.shade.resource.ResourceTransformer;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 import org.codehaus.plexus.logging.console.ConsoleLogger;
-
-import static java.util.Collections.emptyList;
-import static java.util.Collections.emptySet;
-import static java.util.Collections.unmodifiableList;
-import static java.util.Collections.unmodifiableSet;
+import static java.util.Collections.*;
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
+@SuppressWarnings( { "WeakerAccess", "unused" } )
 public final class Shade
-		extends Task {
+  extends Task
+{
+  private File jar;
+  private File uberJar;
+  private Collection<Relocation> relocations;
 
-	private File jar;
-	private File uberJar;
-	private Collection<Relocation> relocations;
+  public void setJar( final File jar )
+  {
+    this.jar = jar;
+  }
 
-	public void setJar(final File jar) {
-		this.jar = jar;
-	}
+  public void setUberJar( final File uberJar )
+  {
+    this.uberJar = uberJar;
+  }
 
-	public void setUberJar(final File uberJar) {
-		this.uberJar = uberJar;
-	}
+  public Relocation createRelocation()
+  {
+    return new Relocation();
+  }
 
-	public Relocation createRelocation() {
-		return new Relocation();
-	}
+  public void addConfiguredRelocation( final Relocation relocation )
+  {
+    if ( relocations == null )
+    {
+      relocations = new ArrayList<>();
+    }
+    relocations.add( relocation );
+  }
 
-	public void addConfiguredRelocation(final Relocation relocation) {
-		if ( relocations == null ) {
-			relocations = new ArrayList<>();
-		}
-		relocations.add(relocation);
-	}
+  @Override
+  public void execute()
+  {
+    try
+    {
+      final Shader shader = new DefaultShader()
+      {{
+        enableLogging( new ConsoleLogger() );
+      }};
+      final ShadeRequest shadeRequest = getShadeRequest();
+      log( "Shading: " + shadeRequest.getJars().stream().map( File::toString ).collect( joining() ) );
+      shader.shade( shadeRequest );
+    }
+    catch ( final IOException | MojoExecutionException ex )
+    {
+      throw new BuildException( ex );
+    }
+  }
 
-	@Override
-	public void execute() {
-		try {
-			final Shader shader = new DefaultShader() {{
-				enableLogging(new ConsoleLogger());
-			}};
-			final ShadeRequest shadeRequest = getShadeRequest();
-			log("Shading: " + shadeRequest.getJars().stream().map(File::toString).collect(joining()));
-			shader.shade(shadeRequest);
-		} catch ( final IOException | MojoExecutionException ex ) {
-			throw new BuildException(ex);
-		}
-	}
+  public abstract static class StringValue
+  {
+    private String value;
 
-	public abstract static class StringValue {
+    public final void setValue( final String value )
+    {
+      this.value = value;
+    }
 
-		private String value;
+    public final String getValue()
+    {
+      return value;
+    }
+  }
 
-		public final void setValue(final String value) {
-			this.value = value;
-		}
+  public static final class Relocation
+  {
 
-		public final String getValue() {
-			return value;
-		}
+    private String pattern;
+    private String shadedPattern;
+    private Collection<Include> includes;
+    private Collection<Exclude> excludes;
+    private boolean isRawString;
 
-	}
+    public void setPattern( final String pattern )
+    {
+      this.pattern = pattern;
+    }
 
-	public static final class Relocation {
+    public void setShadedPattern( final String shadedPattern )
+    {
+      this.shadedPattern = shadedPattern;
+    }
 
-		private String pattern;
-		private String shadedPattern;
-		private Collection<Include> includes;
-		private Collection<Exclude> excludes;
-		private boolean isRawString;
+    public Include createInclude()
+    {
+      return new Include();
+    }
 
-		public void setPattern(final String pattern) {
-			this.pattern = pattern;
-		}
+    public void addConfiguredInclude( final Include include )
+    {
+      if ( includes == null )
+      {
+        includes = new ArrayList<>();
+      }
+      includes.add( include );
+    }
 
-		public void setShadedPattern(final String shadedPattern) {
-			this.shadedPattern = shadedPattern;
-		}
+    public Exclude createExclude()
+    {
+      return new Exclude();
+    }
 
-		public Include createInclude() {
-			return new Include();
-		}
+    public void addConfiguredExclude( final Exclude exclude )
+    {
+      if ( excludes == null )
+      {
+        excludes = new ArrayList<>();
+      }
+      excludes.add( exclude );
+    }
 
-		public void addConfiguredInclude(final Include include) {
-			if ( includes == null ) {
-				includes = new ArrayList<>();
-			}
-			includes.add(include);
-		}
+    public void setRawString( final boolean rawString )
+    {
+      isRawString = rawString;
+    }
 
-		public Exclude createExclude() {
-			return new Exclude();
-		}
+    public static final class Include
+      extends StringValue
+    {
+    }
 
-		public void addConfiguredExclude(final Exclude exclude) {
-			if ( excludes == null ) {
-				excludes = new ArrayList<>();
-			}
-			excludes.add(exclude);
-		}
+    public static final class Exclude
+      extends StringValue
+    {
+    }
+  }
 
-		public void setRawString(final boolean rawString) {
-			isRawString = rawString;
-		}
+  private ShadeRequest getShadeRequest()
+  {
+    final ShadeRequest shadeRequest = new ShadeRequest();
+    shadeRequest.setFilters( prepareFilters() );
+    shadeRequest.setJars( prepareJars() );
+    shadeRequest.setRelocators( prepareRelocations() );
+    shadeRequest.setResourceTransformers( prepareResourceTransformers() );
+    shadeRequest.setUberJar( prepareUberJar() );
+    return shadeRequest;
+  }
 
-		public static final class Include
-				extends StringValue {
-		}
+  private static List<Filter> prepareFilters()
+  {
+    return emptyList();
+  }
 
-		public static final class Exclude
-				extends StringValue {
-		}
+  private Set<File> prepareJars()
+  {
+    final Set<File> files = ofNullable( jar )
+      .map( Collections::singleton )
+      .orElse( emptySet() );
+    return unmodifiableSet( files );
+  }
 
-	}
+  private List<Relocator> prepareRelocations()
+  {
+    final List<Relocator> relocators = relocations
+      .stream()
+      .map( r -> new SimpleRelocator(
+        r.pattern,
+        r.shadedPattern,
+        ofNullable( r.includes )
+          .map( is -> is.stream().map( StringValue::getValue ).collect( toList() ) )
+          .orElse( null ),
+        ofNullable( r.excludes )
+          .map( es -> es.stream().map( StringValue::getValue ).collect( toList() ) )
+          .orElse( null ),
+        r.isRawString
+      ) )
+      .collect( toList() );
+    return unmodifiableList( relocators );
+  }
 
-	private ShadeRequest getShadeRequest() {
-		final ShadeRequest shadeRequest = new ShadeRequest();
-		shadeRequest.setFilters(prepareFilters());
-		shadeRequest.setJars(prepareJars());
-		shadeRequest.setRelocators(prepareRelocations());
-		shadeRequest.setResourceTransformers(prepareResourceTransformers());
-		shadeRequest.setUberJar(prepareUberJar());
-		return shadeRequest;
-	}
+  private static List<ResourceTransformer> prepareResourceTransformers()
+  {
+    return emptyList();
+  }
 
-	private static List<Filter> prepareFilters() {
-		return emptyList();
-	}
-
-	private Set<File> prepareJars() {
-		final Set<File> files = ofNullable(jar)
-				.map(Collections::singleton)
-				.orElse(emptySet());
-		return unmodifiableSet(files);
-	}
-
-	private List<Relocator> prepareRelocations() {
-		final List<Relocator> relocators = relocations
-				.stream()
-				.map(r -> new SimpleRelocator(
-						r.pattern,
-						r.shadedPattern,
-						ofNullable(r.includes)
-								.map(is -> is.stream().map(StringValue::getValue).collect(toList()))
-								.orElse(null),
-						ofNullable(r.excludes)
-								.map(es -> es.stream().map(StringValue::getValue).collect(toList()))
-								.orElse(null),
-						r.isRawString
-				))
-				.collect(toList());
-		return unmodifiableList(relocators);
-	}
-
-	private static List<ResourceTransformer> prepareResourceTransformers() {
-		return emptyList();
-	}
-
-	private File prepareUberJar() {
-		return uberJar;
-	}
-
+  private File prepareUberJar()
+  {
+    return uberJar;
+  }
 }
